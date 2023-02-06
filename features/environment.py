9de4cb79623638cb576fda372ed2023aec07c22e
Eliezer Azevedo <85752394+eliezerdeazevedo@users.noreply.github.com>
2023-02-06 17:11:48 -0300

    Add files via upload

---
+++ features/environment.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+def before_all(context):
+        context.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+caps = {"platformName": "Android", "appium:platformVersion": "6.0", "appium:deviceName": "Android 6.0 x86",
+        "appium:automationName": "uiautomator2", "appium:appPackage": "com.android.calculator2",
+        "appium:appActivity": "com.android.calculator2.Calculator", "appium:ensureWebviewsHavePages": True,
+        "appium:nativeWebScreenshot": True, "appium:newCommandTimeout": 3600, "appium:connectHardwareKeyboard": True}
